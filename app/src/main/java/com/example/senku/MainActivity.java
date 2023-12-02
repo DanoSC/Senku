fa53ec0dbb643902c4bb423bf902593149e8493e
DanoSC <danisastrecarrasco@gmail.com>
2023-12-02 21:00:00 +0100

    new

--- app/src/main/java/com/example/senku/MainActivity.java
+++ app/src/main/java/com/example/senku/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.senku;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botones [][];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
